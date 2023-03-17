01d31a04b61087e84907d601319ca2f501f487d9
GabeBolton <jb.gabe@gmail.com>
2023-03-17 11:09:04 +1100

    initial comment

---
+++ README.md
@@ -0,0 +1,11 @@
+# ANUMC Wall Booking iCalendar
+
+Download `anumc-wall-booking.ics` and import it into a new calendar on your preferred system.
+
+Probably don't import it into your main calendar, make a new one that can be deleted if there are any updates.
+
+Ideally we'd host this as a url calendar, but this'll do for now.
+
+There are some potential feature adds, like combining concurrent events and parsing from the facility name column to more readable names like 'Old Wall'/'New Wall'/'Both Walls'.
+
+Happy sends
---
+++ main.py
@@ -0,0 +1,28 @@
+from tabula import read_pdf
+from ics import Calendar, Event
+import re
+import pandas as pd
+df = read_pdf("2023 Yearly Booking Schedule - ANU Mountaineering Club.pdf", pages="all")
+
+df = pd.concat(df)
+df = df[(df['Date'] != 'TOTAL:')] # remove the final total hours entry
+
+df['Start Time'] = pd.to_datetime(df['Date'].str.lstrip('MoWeFr ') + ' ' + df['From Time'])
+df['Start Time'] = df['Start Time'].dt.tz_localize('Australia/ACT')
+df['End Time'] = pd.to_datetime(df['Date'].str.lstrip('MoWeFr ') + ' ' + df['To Time'])
+df['End Time'] = df['End Time'].dt.tz_localize('Australia/ACT')
+
+c = Calendar()
+
+for index, row in df.iterrows():
+    e = Event()
+    e.name = row['Facility Name'].removeprefix('Badminton\r')
+    e.begin = row['Start Time']
+    e.end = row['End Time']
+    c.events.add(e)
+    c.events
+
+with open('anumc-wall-booking.ics', 'w') as my_file:
+    my_file.writelines(c.serialize_iter())
+
+pass
